0ef7911d051852c52e124e2092be757de7470880
dingluoneu <dingluo.bupt@gmail.com>
2017-11-07 21:39:40 -0500

    01- Drum Kit.

--- demo/views.py
+++ demo/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    return render(request, 'base.html', {})
+
+
+def demo1(request):
+    return render(request, 'demo/demo1.html', {})
